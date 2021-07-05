30aa7eccd90b52ab4abe55cf90b78243cd3be5b9
DavidM42 <davidmerz@gmx.de>
2021-07-05 19:10:26 +0200

    chore: zip release script changes

--- zip_release.sh
+++ zip_release.sh
@@ -1,2 +1,2 @@
-zip -r -FS ../E2NCalculator.zip * -x *.git/* -x node_modules/\* -x .vscode/* -x *.md -x *.sh -x assets/\* -x web-ext-artifacts/\*
-# mv ../E2NCalculator.zip ../E2NCalculator.xpi
+zip -r -FS ../N2E2.zip * -x *.git/* -x node_modules/\* -x .vscode/* -x *.md -x *.sh -x assets/\* -x web-ext-artifacts/\* -x .jshintrc
+# mv ../N2E2.zip ../N2E2.xpi
